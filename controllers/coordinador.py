b33a2ba2c4d2443746c25901092b351e0c2f764d
camiloarcila <camiloarcila05@gmail.com>
2016-05-28 08:48:49 -0500

    cambios 3

--- controllers/coordinador.py
+++ controllers/coordinador.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask.helpers import flash
-from dao.TrabajoGrado_dao import TrabajoGradoDao
+from dao.trabajoDeGrado_dao import TrabajoGradoDao
 from dto.trabajoGrado import TrabajoGrado
 
 
@@ -9,21 +9,22 @@ class CoordinadorController:
         pass
 
     def get_view_nombreT(self):
-        return render_template("coordinador/nombreT.html")
+        return render_template("/coordinador/nombreT.html")
 
     def consulta_nombreT(self, nombre):
-        trabajoG = TrabajoGrado ("",nombre,"","","","","","","","","","","","","","","","","","","","")
+        trabajoG = TrabajoGrado ("","",nombre,"","","","","","","","","","","","","","","","")
 
-        if (TrabajoGradoDao.get_trabajo_titulo(trabajoG) is not None):
+        if (TrabajoGradoDao().get_trabajo_titulo(trabajoG) is not None):
 
-         trabajosDeGrado = TrabajoGrado.getTitulo(trabajoG)
-         return render_template("coordinador/nombreT.html", trabajosDeGrado=trabajosDeGrado)
+
+         trabajosDeGrado = TrabajoGradoDao().get_trabajo_titulo(trabajoG)
+         return render_template("/coordinador/nombreT.html", trabajosDeGrado=trabajosDeGrado)
 
         else:
 
          flash("No existen Trabajos con esos parametros.", "error")
 
-        return render_template("coordinador/nombreT.html")
+        return render_template("/coordinador/nombreT.html")
 
 
 
