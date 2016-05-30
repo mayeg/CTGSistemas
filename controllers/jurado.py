04b4ae7fbcd4d12da1c6a0e1e85736c0bffb01ef
Alejandra Bohorquez <malejitabm@hotmail.com>
2016-05-30 16:01:41 -0500

    Update jurado.py

--- controllers/jurado.py
+++ controllers/jurado.py
@@ -1,8 +1,11 @@
+import hashlib
+from _hashlib import new
 
 from dao.acta_dao import ActaDao
 from dao.tipo_usuario_dao import TipoUsuarioDao
 from dao.usuario_dao import UsuarioDao
 from dao.propuesta_dao import PropuestaDao
+from dao.trabajoDeGrado_dao import TrabajoGradoDao
 from dto.acta import Acta
 from flask.helpers import flash, url_for
 from flask import render_template, redirect, url_for, session
@@ -14,16 +17,26 @@ class JuradoController:
         pass
 
     def get_view_consultar_propuesta(self):
-        tipo = session['usuario']['tipo']
-        usuario = Usuario(nombres=session['usuario']['nombres'],
-                          tipo_usuario=tipo)
-        return render_template("jurado/consulta_propuesta.html", usuario=usuario)
+        cod = session['usuario']['codigo']
+        usuario = Usuario(nombres=session['usuario']['nombres'], codigo=cod)
+        propuestas = PropuestaDao().get_propuesta_consulta_jurado(usuario)
+        if (propuestas != ""):
+            return render_template("jurado/consulta_propuesta.html", usuario=usuario, propuestas=propuestas)
+        else:
+         flash("No existen Propuestas a cargo.", "error")
+         return render_template("jurado/consulta_propuesta.html", usuario=usuario)
+
 
     def get_view_consultar_trabajo(self):
-        tipo = session['usuario']['tipo']
-        usuario = Usuario(nombres=session['usuario']['nombres'],
-                          tipo_usuario=tipo)
+        cod = session['usuario']['codigo']
+        usuario = Usuario(nombres=session['usuario']['nombres'], codigo=cod)
+        trabajos = TrabajoGradoDao().get_trabajo_consulta_jurado(usuario)
+        if (trabajos != ""):
+            return render_template("jurado/consulta_trabajo.html", usuario=usuario, trabajos=trabajos)
+        else:
+            flash("No existen Trabajos a cargo.", "error")
         return render_template("jurado/consulta_trabajo.html", usuario=usuario)
+
 
     def get_view_consultar_sustentaciones(self):
         tipo = session['usuario']['tipo']
