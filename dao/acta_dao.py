41a9dd49c5b039e39e559ee48917d116eb42e4dc
Kingmaq12 <cvasquez647@gmail.com>
2016-05-31 02:16:28 -0500

    Update acta_dao.py

--- dao/acta_dao.py
+++ dao/acta_dao.py
@@ -15,8 +15,7 @@ class ActaDao:
             data = self.__cur.fetchone()
             if data is None:
                 return None
-            return Acta(codigo=data[0], titulo=data[1], tipo=data[2],
-                        fecha=data[3],
+            return Acta(codigo=data[0], titulo=data[1], tipo=data[2], fecha=data[3],
                         archivo=data[4], descripcion=data[5])
         except Exception as e:
             print e.message
@@ -37,14 +36,13 @@ class ActaDao:
             print e.message
             return False
 
-    def modificar_acta(self, titulo_acta, acta):
+    def modificar_acta(self,titulo_acta, acta):
         try:
             query = "UPDATE acta SET codigo= %s, titulo= %s, tipo= %s," \
                     "fecha= %s, documento= %s, Descripcion = %s WHERE titulo=%s "
 
-            param = (
-            acta.getCodigo(), acta.getTitulo(), acta.getTipo(), acta.getFecha(),
-            acta.getArchivo(), acta.getDescripcion(), titulo_acta)
+            param = (acta.getCodigo(),acta.getTitulo(), acta.getTipo(), acta.getFecha(),
+                     acta.getArchivo(), acta.getDescripcion(),titulo_acta)
             self.__cur.execute(query, param)
             self.__conn.commit()
             return True
@@ -53,11 +51,11 @@ class ActaDao:
             print e.message
             return False
 
+
     def get_acta_consulta(self, acta):
 
         # SOLO FECHA
-        if (
-                    acta.getTitulo() == "" and acta.getTipo() == "<-- No Selected -->" and acta.getFecha() != ""):
+        if (acta.getTitulo() == "" and acta.getTipo() == "<-- No Selected -->" and acta.getFecha() != ""):
             try:
                 query = "SELECT * FROM acta WHERE fecha = %s"
                 param = (acta.getFecha(),)
@@ -67,8 +65,7 @@ class ActaDao:
                 if data is None:
                     return []
                 for acta in data:
-                    act = Acta(codigo=acta[0], titulo=acta[1], tipo=acta[2],
-                               fecha=acta[3], archivo=acta[4],
+                    act = Acta(codigo=acta[0], titulo=acta[1], tipo=acta[2], fecha=acta[3], archivo=acta[4],
                                descripcion=acta[5])
                     resultado.append(act)
                 return resultado
@@ -77,8 +74,7 @@ class ActaDao:
                 return []
 
         # SOLO TITULO
-        elif (
-                    acta.getTitulo() != "" and acta.getTipo() == "<-- No Selected -->" and acta.getFecha() == ""):
+        elif (acta.getTitulo() != "" and acta.getTipo() == "<-- No Selected -->" and acta.getFecha() == ""):
 
             try:
                 query = "SELECT * FROM acta WHERE titulo = %s"
@@ -89,8 +85,7 @@ class ActaDao:
                 if data is None:
                     return []
                 for acta in data:
-                    act = Acta(codigo=acta[0], titulo=acta[1], tipo=acta[2],
-                               fecha=acta[3], archivo=acta[4],
+                    act = Acta(codigo=acta[0], titulo=acta[1], tipo=acta[2], fecha=acta[3], archivo=acta[4],
                                descripcion=acta[5])
                     resultado.append(act)
                 return resultado
@@ -98,19 +93,29 @@ class ActaDao:
                 print e.message
                 return []
 
-        elif (
-                    acta.getTitulo() == "" and acta.getTipo() != "" and acta.getFecha() == ""):
-            return None
-        elif (
-                    acta.getTitulo() == "" and acta.getTipo() != "" and acta.getFecha() != ""):
+        elif (acta.getTitulo() == "" and acta.getTipo() != "" and acta.getFecha() == ""):
+            try:
+                query = "SELECT * FROM acta WHERE tipo = %s"
+                param = (acta.getTipo(),)
+                self.__cur.execute(query, param)
+                data = self.__cur.fetchall()
+                resultado = list()
+                if data is None:
+                    return []
+                for acta in data:
+                    act = Acta(codigo=acta[0], titulo=acta[1], tipo=acta[2], fecha=acta[3], archivo=acta[4],
+                               descripcion=acta[5])
+                    resultado.append(act)
+                return resultado
+            except Exception as e:
+                print e.message
+                return []
+        elif (acta.getTitulo() == "" and acta.getTipo() != "" and acta.getFecha() != ""):
             return None
 
-        elif (
-                    acta.getTitulo() != "" and acta.getTipo() == "" and acta.getFecha() != ""):
+        elif (acta.getTitulo() != "" and acta.getTipo() == "" and acta.getFecha() != ""):
             return None
-        elif (
-                    acta.getTitulo() != "" and acta.getTipo() != "" and acta.getFecha() == ""):
+        elif (acta.getTitulo() != "" and acta.getTipo() != "" and acta.getFecha() == ""):
             return None
-        elif (
-                    acta.getTitulo() != "" and acta.getTipo() != "" and acta.getFecha() != ""):
+        elif (acta.getTitulo() != "" and acta.getTipo() != "" and acta.getFecha() != ""):
             return None
